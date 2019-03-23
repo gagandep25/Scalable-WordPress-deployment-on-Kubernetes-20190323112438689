5f2e2680ac4986da9bbb21e8b217ec16b0005855
gagandep25 <48856980+gagandep25@users.noreply.github.com>
2019-03-23 09:30:18 -0400

    debug...still...

--- scripts/deploy-to-bluemix/install_bx.sh
+++ scripts/deploy-to-bluemix/install_bx.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Download IBM Cloud CLI"
-curl -fsSL https://clis.ng.bluemix.net/install/linux | sh
 
 echo "Install kubectl"
 wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"/bin/linux/amd64/kubectl
