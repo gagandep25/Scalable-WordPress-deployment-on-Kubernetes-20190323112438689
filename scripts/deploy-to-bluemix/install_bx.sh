c817366ed99cb4f875f1b0b38ee1bc326eaae5d0
gagandep25 <48856980+gagandep25@users.noreply.github.com>
2019-03-23 09:22:11 -0400

    Debug

--- scripts/deploy-to-bluemix/install_bx.sh
+++ scripts/deploy-to-bluemix/install_bx.sh
@@ -15,7 +15,7 @@ echo "Install kubectl"
 wget --quiet --output-document=/tmp/Bluemix_CLI/bin/kubectl  https://storage.googleapis.com/kubernetes-release/release/"$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"/bin/linux/amd64/kubectl
 chmod +x /tmp/Bluemix_CLI/bin/kubectl
 
-if [ -n "$DEBUG" ]; then
+
   bx --version
   bx plugin list
-fi
+
